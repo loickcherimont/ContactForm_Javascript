f226fab80493b91ac6e7b220c29b3f0eb4a66173
loickcherimont <loickcherimont@gmail.com>
2024-07-31 07:53:05 +0200

    Change validation.js with index.js

--- js/validation.js
+++
@@ -1,6 +0,0 @@
-import { contactForm } from "./components.js";
-import { preProcessing } from "./functions.js";
-
-/** Control fields before send to server */
-contactForm.addEventListener('submit', preProcessing);
-
